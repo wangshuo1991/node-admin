8bc71629f50ce1b86d882be4b45157fa9c8b304c
wangshuo <shuowang1991@163.com>
2018-12-20 14:42:02 +0800

    完成新增成员 重定向到首页 刷新列表

--- outerMethods.js
+++ outerMethods.js
@@ -3,12 +3,35 @@ const fs = require('fs');
 
 let dbPath = './db.json';
 
+// 渲染首页
 exports.find = (callback)=>{
     fs.readFile(dbPath,'utf8',(err,data)=>{
         if (err) {
             return callback(err)
         }
         callback(null,JSON.parse(data).members);
+    });
+}
+
+// 添加新成员 
+exports.add = (member,callback)=>{
+    fs.readFile(dbPath,'utf8',(err,data)=>{
+        if (err) {
+            return callback(err)
+        }
+        let members = JSON.parse(data).members;
+        member.id = members[members.length - 1].id + 1;
+        members.push(member);
+
+        let fileData = JSON.stringify({
+            members: members
+        });
+        fs.writeFile(dbPath,fileData,(err)=>{
+            if (err) {
+                return callback(err);
+            }
+            callback(null);
+        });
     });
 }
 
--- router.js
+++ router.js
@@ -6,6 +6,7 @@ const outerMethods = require('./outerMethods');
 
 const router = express.Router();
 
+// -> '/' 渲染首页
 router.get('/',(req,res)=>{
     outerMethods.find((err,members)=>{
         if (err) {
@@ -17,6 +18,29 @@ router.get('/',(req,res)=>{
         });
     });
 });
+
+// 渲染 新成员的页面
+router.get('/members/add',(req,res)=>{
+    res.render('edit.html');
+});
+
+// 添加新成员
+
+router.post('/members/add',(req,res)=>{
+
+    let content = req.body;
+    
+    outerMethods.add(content,(err)=>{
+        if (err) {
+            return res.status(500).send('server error');
+        }
+        res.redirect('/');
+    });
+
+})
+
+
+
 
 
 // 记得导出路由
